42275ad49335de050da64aff41a704d5120e14a7
weichen0530 <wchen0530@gmail.com>
2021-12-05 18:48:07 -0500

    DP for 120 triangle

--- C#/120. Triangle.cs
+++ C#/120. Triangle.cs
@@ -13,4 +13,44 @@ public class Solution {
         memo.Add(key, Math.Min(left, right) + triangle[x][y]);
         return memo[key];
     }
+	
+	// DP Bottom To Top
+	public int MinimumTotal(IList<IList<int>> triangle) {
+        int n = triangle.Count;
+        if(n == 1) return triangle[0][0]; 
+        int[,] dp = new int[n,n];
+        for(int i = 0; i < n; i++){
+            dp[n-1,i] = triangle[n-1][i];
+        }
+        for(int i = n-2; i >= 0; i--){
+            for(int j = 0; j <= i; j++){
+                dp[i,j] = Math.Min(dp[i+1,j], dp[i+1,j+1])+triangle[i][j];
+            }
+        }
+        return dp[0,0];
+    }
+	
+	// DP Top To Bottom
+	public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle.size() == 1) return triangle.get(0).get(0);
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+        dp[0][0] = triangle.get(0).get(0);
+        for(int i = 1; i < n; i++){
+            dp[i][0] = dp[i-1][0] + triangle.get(i).get(0);
+            dp[i][i] = dp[i-1][i-1] + triangle.get(i).get(i);
+        }
+        if(n > 2){
+            for(int i = 2; i <n ; i++){
+                for(int j = 1; j <= i-1; j++){
+                    dp[i][j] = Math.min(dp[i-1][j-1], dp[i-1][j]) + triangle.get(i).get(j);
+                }
+            }
+        }
+        int result = dp[n-1][0];
+        for(int i = 1; i < n; i++){
+            result = Math.min(result, dp[n-1][i]);
+        }
+        return result;
+    }
 }
--- Java/120. Triangle.java
+++ Java/120. Triangle.java
@@ -13,4 +13,49 @@ class Solution {
         memo.put(key, Math.min(left, right) + triangle.get(x).get(y));
         return memo.get(key);
     }
+	
+	// DP Bottom To Top
+	// only one situation 
+	public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle.size() == 1) return triangle.get(0).get(0);
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+        for(int i = 0; i < n; i++){
+            dp[n-1][i] = triangle.get(n-1).get(i);
+        }
+        for(int i = n-2; i >= 0; i--){
+            for(int j = 0; j <= i; j++){
+                dp[i][j] = Math.min(dp[i+1][j], dp[i+1][j+1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0][0];
+    }
+	
+	// DP Top To Bottom
+	// Need to Handle 3 cases
+	// left most nodes
+	// right most nodes
+	// regular nodes
+	public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle.size() == 1) return triangle.get(0).get(0);
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+        dp[0][0] = triangle.get(0).get(0);
+        for(int i = 1; i < n; i++){
+            dp[i][0] = dp[i-1][0] + triangle.get(i).get(0);
+            dp[i][i] = dp[i-1][i-1] + triangle.get(i).get(i);
+        }
+        if(n > 2){
+            for(int i = 2; i <n ; i++){
+                for(int j = 1; j <= i-1; j++){
+                    dp[i][j] = Math.min(dp[i-1][j-1], dp[i-1][j]) + triangle.get(i).get(j);
+                }
+            }
+        }
+        int result = dp[n-1][0];
+        for(int i = 1; i < n; i++){
+            result = Math.min(result, dp[n-1][i]);
+        }
+        return result;
+    }
 }
