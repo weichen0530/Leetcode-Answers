67f722b8ecfc158a78c4cfb9c8ccc37c87c3f2f2
weichen0530 <wchen0530@gmail.com>
2021-12-05 22:31:11 -0500

    62 unique paths DP bottom to top

--- Java/62. Unique Paths.java
+++ Java/62. Unique Paths.java
@@ -1,5 +1,6 @@
 // DP Top To Bottom
 public int uniquePaths(int m, int n) {
+	// state: dp[i,j} represent 0,0 to i,j unique paths 
 	int[][] dp = new int[m][n];
 	for(int i = 0; i < m; i++){
 		dp[i][0] = 1;
@@ -13,4 +14,22 @@ public int uniquePaths(int m, int n) {
 		}
 	}
 	return dp[m-1][n-1];
+}
+
+// DP Top To Bottom
+public int uniquePaths(int m, int n) {
+	// state: dp[i,j} represent i,j to m-1, n-1 unique paths 
+	int[][] dp = new int[m][n];
+	for(int i = 0; i < m; i++){
+		dp[i][n-1] = 1;
+	}
+	for(int j = 0; j < n; j++){
+		dp[m-1][j] = 1;
+	}
+	for(int i = m-2; i >= 0; i--){
+		for(int j = n-2; j >=0; j--){
+			dp[i][j] = dp[i][j+1]+dp[i+1][j];
+		}
+	}
+	return dp[0][0];
 }
